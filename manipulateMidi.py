adb55a8925f23cbb0afff9d2d09c1ece1606cce3
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-11-08 00:25:47 -0500

    manipulateMidi.py: converted indents to spaces

--- manipulateMidi.py
+++ manipulateMidi.py
@@ -1,30 +1,45 @@
+#!/usr/bin/env python
+
 import mido as md
-import pygame
+import pygame, pygame.midi
+import time
 
-# pygame.init()
-# pygame.midi.init()
+
 def read_midi(track):
-	mozart = md.MidiFile('./MidiFiles/symphony_38_504_1_(c)cvikl.mid')
+    pygame.init()
+    pygame.midi.init()
+    mozart = md.MidiFile('./MidiFiles/symphony_38_504_1_(c)cvikl.mid')
 
-	#set up the output port for the mido data and the input port in pygame
-	# md.get_output_names()
-	# pygame_port = md.backend.pygame
-	# in_port = pygame.midi.input()
-	# out_port = pygame_port.open_output()
-	# in_port.input(out_port)
+    print pygame.midi.get_default_output_id()
+    print pygame.midi.get_device_info(0)
+
+    #set up the output port for the mido data and the input port in pygame
+    # md.get_output_names()
+    # pygame_port = md.backend.pygame
+    # in_port = pygame.midi.input()
+    player = pygame.midi.Output(0)
+    player.set_instrument(0, 1)
+    print('playing')
+    player.note_on(64, 277,1)
+    time.sleep(1)
+    player.note_off(64,277,1)
+    print('played')
+
+    # in_port.input(out_port)
 
 
-	for message in mozart.play():
-		print(message)
-		# out_port.send(message)
+    # for message in mozart.play():
+    #   print(message)
+    #   out_port.send(message)
+    #   # out_port.send(message)
 
-	# for i, track in enumerate(mozart.tracks):
-		# print('Track {}: {}'.format(i, track.name))
-		# print track.name
-		# for message in track:
-			# print(message)
+    # for i, track in enumerate(mozart.tracks):
+        # print('Track {}: {}'.format(i, track.name))
+        # print track.name
+        # for message in track:
+            # print(message)
 
-
+    del player
 
 if __name__ == '__main__':
-	read_midi(1)
+    read_midi(1)
