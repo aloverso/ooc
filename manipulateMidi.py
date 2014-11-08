cbd0df862fd11ec4fe55b85582052c61aa24864d
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-11-07 23:38:44 -0500

    maninpulateMidi.py: Commented out port setup because it's not working

--- manipulateMidi.py
+++ manipulateMidi.py
@@ -1,22 +1,22 @@
 import mido as md
 import pygame
 
-pygame.init()
-pygame.midi.init()
+# pygame.init()
+# pygame.midi.init()
 def read_midi(track):
 	mozart = md.MidiFile('./MidiFiles/symphony_38_504_1_(c)cvikl.mid')
 
 	#set up the output port for the mido data and the input port in pygame
-	md.get_output_names()
-	pygame_port = md.backend.pygame
-	in_port = pygame.midi.input()
-	out_port = pygame_port.open_output()
-	in_port.input(out_port)
+	# md.get_output_names()
+	# pygame_port = md.backend.pygame
+	# in_port = pygame.midi.input()
+	# out_port = pygame_port.open_output()
+	# in_port.input(out_port)
 
 
-	for message in mozart.tracks[track]:
-		# print(message)
-		out_port.send(message)
+	for message in mozart.play():
+		print(message)
+		# out_port.send(message)
 
 	# for i, track in enumerate(mozart.tracks):
 		# print('Track {}: {}'.format(i, track.name))
